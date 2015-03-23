dc1103644c8441199832598b7bb8c484f07d6540
KageKeeper <kagekeeper@gmail.com>
2015-03-22 23:23:20 -0700

    This commit primarily implements the functionality of updating the main menu (via Ajax) after a modal change has been submitted and successfully completed.

---
+++ GardenManager.Web/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GardenManager.Web.DataContexts;
+using GardenManager.Web.ViewModels;
+
+namespace GardenManager.Web.Controllers
+{
+    /*
+     * The MenuController is intended to be a repository for any Actions related to the management and upkeep of any menus.
+     */
+    public class MenuController : Controller
+    {
+        private GardenDb db = new GardenDb();
+
+        public ActionResult UpdateMainMenu()
+        {
+            // Create the ViewModel for the menu
+            _NavigationViewModel viewModel = new _NavigationViewModel(db.Gardens.ToList());
+
+            return PartialView("_MainNavigationPartial", viewModel);
+        }
+    }
+}
--- GardenManager.Web/Scripts/GardenHQ.js
+++ GardenManager.Web/Scripts/GardenHQ.js
@@ -29,9 +29,56 @@ $(document).on('click', '#submit-modal-form', function (e) {
             $('#modal-parent').modal('hide');
             LoadAjaxContent(data);
             ShowCompleteMessage();
+            UpdateMainMenu();
         }
     });
 
+});
+
+// Handle the drop down menu functionality.
+$(document).on('click', '.main-menu').on('click', 'a', function (e) {
+    var parents = $(this).parents('li');
+    var li = $(this).closest('li.dropdown');
+    var otherItems = $('.main-menu li').not(parents);
+    otherItems.find('a').removeClass('active');
+    otherItems.find('a').removeClass('active-parent');
+    if ($(this).hasClass('dropdown-toggle') || $(this).closest('li').find('ul').length == 0) {
+        $(this).addClass('active-parent');
+        var current = $(this).next();
+        if (current.is(':visible')) {
+            li.find("ul.dropdown-menu").slideUp('fast');
+            li.find("ul.dropdown-menu a").removeClass('active')
+        }
+        else {
+            otherItems.find("ul.dropdown-menu").slideUp('fast');
+            current.slideDown('fast');
+        }
+    }
+    else {
+        if (li.find('a.dropdown-toggle').hasClass('active-parent')) {
+            var pre = $(this).closest('ul.dropdown-menu');
+            pre.find("li.dropdown").not($(this).closest('li')).find('ul.dropdown-menu').slideUp('fast');
+        }
+    }
+    if ($(this).hasClass('active') == false) {
+        $(this).parents("ul.dropdown-menu").find('a').removeClass('active');
+        $(this).addClass('active')
+    }
+    if ($(this).hasClass('ajax-link')) {
+        e.preventDefault();
+        if ($(this).hasClass('add-full')) {
+            $('#content').addClass('full-content');
+        }
+        else {
+            $('#content').removeClass('full-content');
+        }
+        var url = $(this).attr('href');
+        //window.location.hash = url;
+        LoadAjaxContent(url);
+    }
+    if ($(this).attr('href') == '#') {
+        e.preventDefault();
+    }
 });
 
 //////////////////////////////////////////////////////
@@ -45,7 +92,6 @@ $(document).ready(function () {
     $('.show-sidebar').on('click', function (e) {
         e.preventDefault();
         $('div#main').toggleClass('sidebar-show');
-        setTimeout(MessagesMenuWidth, 250);
     });
     var ajax_url = location.hash.replace(/^#/, '');
     if (ajax_url.length < 1) {
@@ -53,51 +99,7 @@ $(document).ready(function () {
     }
     LoadAjaxContent(ajax_url);
 
-    // Handle the drop down menu functionality.
-    $('.main-menu').on('click', 'a', function (e) {
-        var parents = $(this).parents('li');
-        var li = $(this).closest('li.dropdown');
-        var otherItems = $('.main-menu li').not(parents);
-        otherItems.find('a').removeClass('active');
-        otherItems.find('a').removeClass('active-parent');
-        if ($(this).hasClass('dropdown-toggle') || $(this).closest('li').find('ul').length == 0) {
-            $(this).addClass('active-parent');
-            var current = $(this).next();
-            if (current.is(':visible')) {
-                li.find("ul.dropdown-menu").slideUp('fast');
-                li.find("ul.dropdown-menu a").removeClass('active')
-            }
-            else {
-                otherItems.find("ul.dropdown-menu").slideUp('fast');
-                current.slideDown('fast');
-            }
-        }
-        else {
-            if (li.find('a.dropdown-toggle').hasClass('active-parent')) {
-                var pre = $(this).closest('ul.dropdown-menu');
-                pre.find("li.dropdown").not($(this).closest('li')).find('ul.dropdown-menu').slideUp('fast');
-            }
-        }
-        if ($(this).hasClass('active') == false) {
-            $(this).parents("ul.dropdown-menu").find('a').removeClass('active');
-            $(this).addClass('active')
-        }
-        if ($(this).hasClass('ajax-link')) {
-            e.preventDefault();
-            if ($(this).hasClass('add-full')) {
-                $('#content').addClass('full-content');
-            }
-            else {
-                $('#content').removeClass('full-content');
-            }
-            var url = $(this).attr('href');
-            //window.location.hash = url;
-            LoadAjaxContent(url);
-        }
-        if ($(this).attr('href') == '#') {
-            e.preventDefault();
-        }
-    });
+    
 
     //var height = window.innerHeight - 49;
     //$('#main').css('min-height', height)
@@ -205,6 +207,20 @@ function LoadAjaxContent(url) {
 }
 
 function ShowCompleteMessage() {
-    $('.notification-message-success').html("Update Completed Successfully!");
     $('.notification-message-success').slideToggle(400).delay(3000).slideToggle(400);
+}
+
+function UpdateMainMenu() {
+    $.ajax({
+        mimetype: 'text/html; charset=utf-8',
+        url: '/Menu/UpdateMainMenu',
+        type: 'GET',
+        success: function (data) {
+            $('#sidebar-left').html(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        },
+        dataType: "html"
+    });
 }
